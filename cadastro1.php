027b0b7f6ba38294e6b10f66ead43858ceec07eb
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-23 20:56:23 -0300

    Estilizar css, bug fix php

--- cadastro1.php
+++ cadastro1.php
@@ -1,6 +1,6 @@
 <?php
-   require_once '../usarios.php';
-   $u = new Usuario;
+   // require_once '../usarios.php';
+   // $u = new Usuario;
 ?>
 <html lang="pt-br">
 <head>
@@ -9,66 +9,69 @@
 	<link rel="stylesheet" href="estilo.css">
 <head>
 <body>
- <div id="corpo-form" action="cadastro.php">
-	<h1> Cadatrar </h1>
-	<form method="POST" >
-	<input type="text"      placeholder="Nome Completo"  name="nome"       maxlength="30">
-	<input type="telephone" placeholder="Telefone"       name="telefone"   maxlength="15">
-	<input type="email"     placeholder="Email"          name="email"      maxlength="40">
-	<input type="text"      placeholder="Endereço"       name="endereco"   maxlength="100">
-	<input type="password"  placeholder="Senha"          name="senha"      maxlength="15">
-	<input type="password"  placeholder="ConfirmarSenha" name="confsenha"  maxlength="15">
-    <select  placeholder="Você é:" name=" funcao" >
-      <option value="Dono">Dono</option>
-      <option value="Cuidador">Cuidador</option>
-      <option value="Ambos">Ambos</option>
-    </select>
-	<input type="submit" value="CADASTRAR">	
-	</form>
- </div>
- <?php
-//verificar se clicou no botão 
-if (isset($_POST['nome']))
-{
-$nome = addslashes ($_POST['nome']);
-$telefone = addslashes ($_POST['telefone']);
-$email = addslashes ($_POST['email']);
-$endereco = addslashes ($_POST['endereco']);
-$senha = addslashes ($_POST['senha']);
-$confirmarSenha = addslashes ($_POST['confsenha']);
-$funcao = addslashes ($_POST['funcao']);
-//verificar se estar  preenchido
-if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($endereco) && !empty($senha) && !empty($confrmarSenha) && !empty($funcao))
-{
- $u -> conectar("carrapicho","localhost","root","");
- if ($u -> msgErro == "") //ok
-{ 
-   if ($senha == $confirmarSenha)
-{
- if($u->cadastrar($nome,$telefone,$email,$endereco,$senha,$funcao))
-  {
-     echo "Cadastrado com sucesso!Faça o login para entrar!";
-  }
- else
- {
-    echo "Email já está cadastrado!";
- }
-}
+   <div class="container">
 
-  else
-{
-  echo "Senha e conrmar senha nao correspondem!";
-}
-}
-else
-{
-   echo "Erro:".$u->msgErro;
-}
-}else 
-{
-echo "Preencha todos os campos!";
-}
-}
+      <div id="corpo-form" action="cadastro.php">
+         <h1> Cadastrar </h1>
+	<form method="POST" >
+      <input type="text"      placeholder="Nome Completo"  name="nome"       maxlength="30">
+      <input type="telephone" placeholder="Telefone"       name="telefone"   maxlength="15">
+      <input type="email"     placeholder="Email"          name="email"      maxlength="40">
+      <input type="text"      placeholder="Endereço"       name="endereco"   maxlength="100">
+      <input type="password"  placeholder="Senha"          name="senha"      maxlength="15">
+      <input type="password"  placeholder="Confirmar Senha" name="confsenha"  maxlength="15">
+      <select  placeholder="Você é:" name=" funcao" >
+         <option value="Dono">Dono</option>
+         <option value="Cuidador">Cuidador</option>
+         <option value="Ambos">Ambos</option>
+      </select>
+      <input type="submit" value="CADASTRAR">	
+	</form>
+</div>
+</div>
+<?php
+//verificar se clicou no botão 
+// if (isset($_POST['nome']))
+// {
+// $nome = addslashes ($_POST['nome']);
+// $telefone = addslashes ($_POST['telefone']);
+// $email = addslashes ($_POST['email']);
+// $endereco = addslashes ($_POST['endereco']);
+// $senha = addslashes ($_POST['senha']);
+// $confirmarSenha = addslashes ($_POST['confsenha']);
+// $funcao = addslashes ($_POST['funcao']);
+// //verificar se estar  preenchido
+// if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($endereco) && !empty($senha) && !empty($confrmarSenha) && !empty($funcao))
+// {
+//  $u -> conectar("carrapicho","localhost","root","");
+//  if ($u -> msgErro == "") //ok
+// { 
+//    if ($senha == $confirmarSenha)
+// {
+//  if($u->cadastrar($nome,$telefone,$email,$endereco,$senha,$funcao))
+//   {
+//      echo "Cadastrado com sucesso!Faça o login para entrar!";
+//   }
+//  else
+//  {
+//     echo "Email já está cadastrado!";
+//  }
+// }
+
+//   else
+// {
+//   echo "Senha e conrmar senha nao correspondem!";
+// }
+// }
+// else
+// {
+//    echo "Erro:".$u->msgErro;
+// }
+// }else 
+// {
+// echo "Preencha todos os campos!";
+// }
+// }
 
 ?>
 </body> 
--- index.php
+++ index.php
@@ -5,14 +5,16 @@
 	<link rel="stylesheet" href="estilo.css">
 <head>
 <body>
- <div id="corpo-form">
-	<h1> Entrar </h1>
-	<form method="POST" action="processa.php">
-	<input type="email" placeholder="Usuário" maxlength="40">
-	<input type="password" placeholder="Senha"maxlength="15">
-	<input type="submit" value="ACESSAR">	
-	<a href="cadastro1.php">Não é cadastrado?<strong> CADASTRE-SE </strong></a>
-	</form>
+	<div class="container">
+		<div id="corpo-form">
+			<h1> Entrar </h1>
+			<form method="POST" action="processa.php">
+				<input type="email" placeholder="Usuário" maxlength="40">
+				<input type="password" placeholder="Senha"maxlength="15">
+				<input type="submit" value="ACESSAR">	
+				<p id="cadastro">Não é cadastrado?<a href="cadastro1.php"> CADASTRE-SE </a><p>
+					</form>
+				</div>
  </div>
  
 </body> 
