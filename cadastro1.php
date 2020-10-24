16b3f6be826fbf04be094486d1113c649e4990d2
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-23 21:22:15 -0300

    bug fix on require directory

--- cadastro1.php
+++ cadastro1.php
@@ -1,6 +1,6 @@
 <?php
-   // require_once '../usarios.php';
-   // $u = new Usuario;
+   require_once('usuario.php');
+   $u = new Usuario;
 ?>
 <html lang="pt-br">
 <head>
@@ -11,16 +11,16 @@
 <body>
    <div class="container">
 
-      <div id="corpo-form" action="cadastro.php">
+      <div id="corpo-form" action="#">
          <h1> Cadastrar </h1>
-	<form method="POST" >
-      <input type="text"      placeholder="Nome Completo"  name="nome"       maxlength="30">
-      <input type="telephone" placeholder="Telefone"       name="telefone"   maxlength="15">
-      <input type="email"     placeholder="Email"          name="email"      maxlength="40">
-      <input type="text"      placeholder="Endereço"       name="endereco"   maxlength="100">
-      <input type="password"  placeholder="Senha"          name="senha"      maxlength="15">
-      <input type="password"  placeholder="Confirmar Senha" name="confsenha"  maxlength="15">
-      <select  placeholder="Você é:" name=" funcao" >
+	   <form method="POST" >
+            <input type="text"      placeholder="Nome Completo"  name="nome"       maxlength="30">
+            <input type="telephone" placeholder="Telefone"       name="telefone"   maxlength="15">
+            <input type="email"     placeholder="Email"          name="email"      maxlength="40">
+            <input type="text"      placeholder="Endereço"       name="endereco"   maxlength="100">
+            <input type="password"  placeholder="Senha"          name="senha"      maxlength="15">
+            <input type="password"  placeholder="Confirmar Senha" name="confsenha"  maxlength="15">
+            <select  placeholder="Você é:" name=" funcao" >
          <option value="Dono">Dono</option>
          <option value="Cuidador">Cuidador</option>
          <option value="Ambos">Ambos</option>
@@ -30,48 +30,53 @@
 </div>
 </div>
 <?php
-//verificar se clicou no botão 
-// if (isset($_POST['nome']))
-// {
-// $nome = addslashes ($_POST['nome']);
-// $telefone = addslashes ($_POST['telefone']);
-// $email = addslashes ($_POST['email']);
-// $endereco = addslashes ($_POST['endereco']);
-// $senha = addslashes ($_POST['senha']);
-// $confirmarSenha = addslashes ($_POST['confsenha']);
-// $funcao = addslashes ($_POST['funcao']);
-// //verificar se estar  preenchido
-// if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($endereco) && !empty($senha) && !empty($confrmarSenha) && !empty($funcao))
-// {
-//  $u -> conectar("carrapicho","localhost","root","");
-//  if ($u -> msgErro == "") //ok
-// { 
-//    if ($senha == $confirmarSenha)
-// {
-//  if($u->cadastrar($nome,$telefone,$email,$endereco,$senha,$funcao))
-//   {
-//      echo "Cadastrado com sucesso!Faça o login para entrar!";
-//   }
-//  else
-//  {
-//     echo "Email já está cadastrado!";
-//  }
-// }
+// verificar se clicou no botão 
 
-//   else
-// {
-//   echo "Senha e conrmar senha nao correspondem!";
-// }
-// }
-// else
-// {
-//    echo "Erro:".$u->msgErro;
-// }
-// }else 
-// {
+
+if (isset($_POST['nome']))
+{
+   $nome = addslashes ($_POST['nome']);
+   $telefone = addslashes ($_POST['telefone']);
+   $email = addslashes ($_POST['email']);
+   $endereco = addslashes ($_POST['endereco']);
+   $senha = addslashes ($_POST['senha']);
+   $confirmarSenha = addslashes ($_POST['confsenha']);
+   $funcao = addslashes ($_POST['funcao']);
+   //verificar se estar  preenchido
+ 
+   
+   if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($endereco) && !empty($senha) && !empty($confrmarSenha) && !empty($funcao))
+   {
+      var_dump($nome);
+   $u -> conectar("carrapicho","localhost","root","");
+   if ($u -> msgErro == "") //ok
+   { 
+      if ($senha == $confirmarSenha)
+      {
+ if($u->cadastrar($nome,$telefone,$email,$endereco,$senha,$funcao))
+  {
+     echo "Cadastrado com sucesso!Faça o login para entrar!";
+  }
+ else
+ {
+    echo "Email já está cadastrado!";
+ }
+}
+
+  else
+{
+  echo "Senha e conrmar senha nao correspondem!";
+}
+}
+else
+{
+   echo "Erro:".$u->msgErro;
+}
+}else 
+{
 // echo "Preencha todos os campos!";
-// }
-// }
+}
+}
 
 ?>
 </body> 
